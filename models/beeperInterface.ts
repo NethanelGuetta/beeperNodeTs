7b420432ac055a915aa26fa2e1dc89fb4244df2a
NethanelGuetta <netinello@gmail.com>
2024-09-26 09:46:51 +0300

    created all the folders

---
+++ models/beeperInterface.ts
@@ -0,0 +1,9 @@
+export interface Beeper {
+    id: string;
+    name: string;
+    status: string;
+    creationTime: string;
+    boomingTime: string;
+    longitude: number;
+    latitude: number;
+}
